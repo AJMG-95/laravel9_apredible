0b5fd16c4ef41ee7182c71d5cd2881279bf44779
amargue <antoniojesus.marchena@iesdonana.org>
2023-07-28 16:28:40 +0200

    Como utilizar rutas

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+// personal-site.com => welcome
+// personal-site.com =>/contacto => contact
+// personal-site.com/blog => blog
+// personal-site.com/about => about
+
+
+Route::view('/', 'welcome');
+Route::view('/contacto', 'contact');
+Route::view('/blog', 'blog');
+Route::view('/about', 'about');
+
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
