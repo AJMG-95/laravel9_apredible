efc6f7298f0a63679390af6c7a5cd99a0791cd64
amargue <antoniojesus.marchena@iesdonana.org>
2023-08-01 15:00:15 +0200

    rutas con nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,13 @@
     <title>Index</title>
 </head>
 <body>
+
+    <ul>
+        <li><a href="<?= route('home') ?>">Home</a></li>
+        <li><a href="<?= route('blog') ?>">Blog</a></li>
+        <li><a href="<?= route('about') ?>">About</a></li>
+        <li><a href="<?= route('contact') ?>">Contacto</a></li>
+    </ul>
     <h1>Index</h1>
 </body>
 </html>
