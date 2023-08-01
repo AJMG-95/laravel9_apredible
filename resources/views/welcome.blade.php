7914b01d8ea9024ab7a6e015cc00eaa660e8d177
amargue <antoniojesus.marchena@iesdonana.org>
2023-08-01 16:09:46 +0200

    blade y partials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,8 @@
     <title>Index</title>
 </head>
 <body>
-
-    <ul>
-        <li><a href="<?= route('home') ?>">Home</a></li>
-        <li><a href="<?= route('blog') ?>">Blog</a></li>
-        <li><a href="<?= route('about') ?>">About</a></li>
-        <li><a href="<?= route('contact') ?>">Contacto</a></li>
-    </ul>
+    @include('partials.navigation')
     <h1>Index</h1>
 </body>
 </html>
+
