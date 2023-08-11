da515b6aa1269ff3e3eec141c29268f52eaf2819
amargue <antoniojesus.marchena@iesdonana.org>
2023-08-11 16:14:02 +0200

    controladores

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,3 +1,6 @@
 <x-layouts.app title="blog" meta-description="blog meta description">
-    <h1>blog</h1
-</x-layouts.app>
+    <h1>blog</h1 </x-layouts.app>
+
+    @foreach ($posts as $post)
+        <h2> {{ $post['title'] }} </h2>
+    @endforeach
