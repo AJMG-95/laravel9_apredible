e78b6c1d7c47b16cb0ca817783c75fc5803a66a8
amargue <antoniojesus.marchena@iesdonana.org>
2023-08-18 15:17:24 +0200

    eloquent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts= DB::table('posts')->get();
+        $posts= Post::get();
 
         return view('blog', ['posts' => $posts]);
     }
