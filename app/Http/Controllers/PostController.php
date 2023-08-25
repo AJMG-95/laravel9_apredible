01b5f4d48f7f7b1613ebb17258664d75a02f47da
amargue <antoniojesus.marchena@iesdonana.org>
2023-08-25 18:44:54 +0200

    Validacion de formulario

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,6 +28,12 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
+
+        $request->validate([
+            'title' => ['required',  'min:5'],
+            'body' => ['required']
+        ]);
+
         $post = new Post;
 
         $post->title = $request->input('title');
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,15 +1,28 @@
 <x-layouts.app title="Crear nuevo post" meta-description="Formulario para crear un nuevo blog post">
     <h1>Create new post</h1>
 
+
+    {{--     @foreach ($errors->all() as $error)
+        <p>{{ $error }}</p>
+    @endforeach --}}
+
     <form action="{{ route('posts.store') }}" method="POST">
         @csrf
         <label for="">
             Title <br>
-            <input type="text" name="title" id="">
+            <input type="text" name="title" id="" value="{{ old('title') }}">
+            @error('title')
+                <br>
+                <small style="color: red">{{ $message }}</small>
+            @enderror
         </label> <br>
         <label for="">
             Body <br>
-            <textarea name="body" id="" cols="30" rows="10"></textarea>
+            <textarea name="body" id="" cols="30" rows="10">{{ old('body') }}</textarea>
+            @error('body')
+                <br>
+                <small style="color: red">{{ $message }}</small>
+            @enderror
         </label> <br>
         <button type="submit">Enviar</button>
     </form><br>
